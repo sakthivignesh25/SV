15e761b7e8932b497fce833a063d231dc2de722a
sakthivignesh25 <sakthikamal2522@gmail.com>
2025-03-17 19:09:29 +0530

    Create loading.js

---
+++ my -ecommerce-child/loading.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector(".loading-screen").style.display = "none";
+    }, 2000);
+});
