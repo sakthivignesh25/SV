f3f65aaad167cc836518eea73f8963f93ff5ce51
sakthivignesh25 <sakthikamal2522@gmail.com>
2025-03-17 19:02:21 +0530

    Delete loading.js

--- loading.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelector(".loading-screen").style.display = "none";
-    }, 2000);
-});
