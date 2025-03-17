bf88a15a421d438948e1db4be5404fe9c40452a7
sakthivignesh25 <sakthikamal2522@gmail.com>
2025-03-17 19:12:10 +0530

    Create customer-care.php

---
+++ my -ecommerce-child/customer-care.php
@@ -0,0 +1,9 @@
+<?php
+/* Template Name: Customer Care */
+get_header();
+?>
+<div class="customer-care">
+    <h2>Contact Our Support</h2>
+    <p>Email: support@yourstore.com</p>
+</div>
+<?php get_footer(); ?>
